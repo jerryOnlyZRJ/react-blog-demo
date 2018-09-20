704596b0dfd9a444b1b5fe69b910a2a77104e31e
iconie <iconie@tencent.com>
2018-09-20 21:12:24 +0800

    init project

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        main: path.join(__dirname, './src/pages/Main/index'),
+        article: path.join(__dirname, './src/pages/Article/index')
+    },
+    output: {
+        path: path.join(__dirname, './dist'),
+        filename: '[name].min.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.less$/,
+                loader: 'style-loader!css-loader!less-loader'
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {}
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: path.join(__dirname, './index.html'),
+            chunks: ['main']
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'article.html',
+            template: path.join(__dirname, './index.html'),
+            chunks: ['article']
+        })
+    ],
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+    externals: {
+        "react": 'React',
+        'react-dom': 'ReactDOM'
+    }
+}
