29072bb0b5317e0d19eab6274770a44dce073fa4
ranjayzheng <ranjayzheng@qq.com>
2019-01-04 10:29:06 +0800

    add feedback verification

--- src/client/components/FeedbackForm.jsx
+++ src/client/components/FeedbackForm.jsx
@@ -1,27 +1,27 @@
 import React from 'react'
 import axios from 'axios'
-import {preURL, publicURL} from "../config";
+import { preURL, publicURL } from "../config";
 
 import "./FeedbackForm.less"
 import "./ArticleContent.less"
 
-export default class FeedbackForm extends React.Component{
-    constructor(props){
+export default class FeedbackForm extends React.Component {
+    constructor(props) {
         super(props);
         this.state = {
-            form:{
-                nickname:"",
-                mail:"",
-                title:"",
-                detail:""
+            form: {
+                nickname: "",
+                mail: "",
+                title: "",
+                detail: ""
             }
         }
     }
 
-    componentDidMount(){
+    componentDidMount() {
     }
 
-    setForm(obj = {}){
+    setForm(obj = {}) {
         let form = this.state.form;
         let newForm = Object.assign({}, form, obj);
         this.setState({
@@ -29,43 +29,68 @@ export default class FeedbackForm extends React.Component{
         })
     }
 
-    submitFeedback(){
+    submitFeedback() {
         console.log(this.state.form);
 
-        if(Object.keys(this.state.form).filter((key) => !this.state.form[key]).length !== 0){
-            alert('请完善所有内容');
+        function htmlEncode(str) {
+            return String(str)
+                .replace(/&/g, '&amp;')
+                .replace(/"/g, '&quot;')
+                .replace(/'/g, '&#39;')
+                .replace(/</g, '&lt;')
+                .replace(/>/g, '&gt;');
+        }
+
+        const zhForm = {
+            nickname: "昵称",
+            mail: "邮箱",
+            title: "标题",
+            detail: "详情"
+        }
+
+        const emptyKey = Object.keys(this.state.form).filter((key) => !this.state.form[key])
+        if (emptyKey.length !== 0) {
+            alert(`${zhForm[emptyKey[0]]}不能为空`);
             return;
         }
 
-        // TODO: 应该在这里加上对博客字段的判断，主要目的为过滤无效内容，防止攻击注入等。
+        const mailRegExp = /^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/
+        if (!mailRegExp.test(this.state.form['mail'])) {
+            alert('请输入正确邮箱')
+            return
+        }
+        //编码XSS
+        Object.keys(this.state.form).map(key => {
+            this.state.form[key] = htmlEncode(this.state.form[key])
+        })
 
         axios.post(preURL + '/mail', {
-           info: this.state.form
+            info: this.state.form
         })
-            .then( (response) => {
+            .then((response) => {
                 console.log(response);
-                if(response.data.success){
+                if (response.data.success) {
                     this.setState({
-                        form:{
-                            nickname:"",
-                            mail:"",
-                            title:"",
-                            detail:""
+                        form: {
+                            nickname: "",
+                            mail: "",
+                            title: "",
+                            detail: ""
                         }
                     })
-                    if(response.data.success)
+                    if (response.data.success)
                         alert('提交留言成功');
                     else
                         alert('提交留言失败，服务器错误');
                 }
             })
-            .catch( (error) => {
+            .catch((error) => {
                 console.log(error);
             });
     }
 
-    render(){
-        return(
+    render() {
+        return (
             <div className={'post-container'}>
 
                 <div className={'post-title'}>
@@ -74,34 +99,42 @@ export default class FeedbackForm extends React.Component{
 
                 <div className={'form-item'}>
                     <label htmlFor="name">昵称：</label>
-                    <input id={'name'} autoComplete="off" value={this.state.form.nickname} onChange={e => {this.setForm({
-                        nickname: e.target.value
-                    })}}/>
+                    <input id={'name'} autoComplete="off" value={this.state.form.nickname} onChange={e => {
+                        this.setForm({
+                            nickname: e.target.value
+                        })
+                    }} />
                 </div>
 
                 <div className={'form-item'}>
                     <label htmlFor="mail">邮箱：</label>
-                    <input id={'mail'} autoComplete="off" value={this.state.form.mail} onChange={e => {this.setForm({
-                        mail: e.target.value
-                    })}}/>
+                    <input id={'mail'} autoComplete="off" value={this.state.form.mail} onChange={e => {
+                        this.setForm({
+                            mail: e.target.value
+                        })
+                    }} />
                 </div>
 
                 <div className={'form-item'}>
                     <label htmlFor="title">标题：</label>
-                    <input id={'title'} autoComplete="off" value={this.state.form.title} onChange={e => {this.setForm({
-                        title: e.target.value
-                    })}}/>
+                    <input id={'title'} autoComplete="off" value={this.state.form.title} onChange={e => {
+                        this.setForm({
+                            title: e.target.value
+                        })
+                    }} />
                 </div>
 
                 <div className={'form-item'}>
                     <label htmlFor="detail">详情：</label>
-                    <textarea id={'detail'} autoComplete="off" value={this.state.form.detail} onChange={e => {this.setForm({
-                        detail: e.target.value
-                    })}}/>
+                    <textarea id={'detail'} autoComplete="off" value={this.state.form.detail} onChange={e => {
+                        this.setForm({
+                            detail: e.target.value
+                        })
+                    }} />
                 </div>
 
                 <div className={'form-item'}>
-                    <button onClick={() => {this.submitFeedback()}}>提交留言</button>
+                    <button onClick={() => { this.submitFeedback() }}>提交留言</button>
                 </div>
 
             </div>
